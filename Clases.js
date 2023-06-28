6e2571c28e93391a9af0112fa9027c0f72a0cb94
NicolasMaxOliveira <nicolasmaxoliveira@gmail.com>
2023-06-28 11:21:46 -0300

    "Commit"

--- Clases.js
+++ Clases.js
@@ -7,4 +7,23 @@ function Auto(puertas, color, marca, año, ruedas) {
     this.año = año;
     this.ruedas = ruedas;
 }
+let miPrimerAuto = new Auto(2, 'Verde', 'Ferrari', '2015', 4);
+console.log(miPrimerAuto);
+console.log(miPrimerAuto.marca);
 
+// EXPRESION DE CLASE: forma actual de declarar una clase.
+// Las CLASES tienen 2 tipos de datos: propiedades y métodos. Abajo son propiedades.
+// METODOS son funciones dentro de la clase.
+class Auto{
+    constructor(puertas, color, marca, año, ruedas) {
+        this.puertas = puertas;
+        this.color = color;
+        this.marca = marca;
+        this.año = año;
+        this.ruedas = ruedas;
+    }
+}
+
+let miSegundoAuto = new Auto(4, 'Blanco', 'Fiat', 2015, 4);
+console.log(miSegundoAuto);
+console.log(miSegundoAuto.marca);
