37f624effd399239d45f83b21036cef9e4a86c01
NicolasMaxOliveira <nicolasmaxoliveira@gmail.com>
2023-07-03 18:44:54 -0300

    modulo 8 terminado

--- Callbacks.js
+++ Callbacks.js
@@ -12,3 +12,27 @@ function saludar(cb1, cb2) {
  var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
  console.log(resultado);
 
+ //Nuevo ejemplo callback
+ var devuelvoFrase = function (comida) {
+    return 'Hoy quiero comer: ' + comida;
+ };
+
+var hablar = function (comida, cb) {
+    return cb(comida);
+};
+
+var fraseFinal = hablar('Pizza', devuelvoFrase)
+console.log(fraseFinal);
+
+//OTRO ejemplo callback
+function primero (segundo) {
+    setTimeout(function () {
+        console.log("Primero");
+        segundo();
+    }, 3000);
+}
+function segundo () {
+    console.log("Segundo");
+}
+primero(segundo);
+
